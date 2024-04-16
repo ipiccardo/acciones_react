5345a787d602a239b946886479358c0b908bce30
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-16 20:41:34 -0300

    First commit

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useRouter, useSearchParams, redirect } from "next/navigation";
+
+export default function SearchBar() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const query = event.currentTarget.query.value;
+        router.push(`/?q=${query}`);
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit} className="inline-flex gap-2 mb-4">
+            <input className="px-2 border-solid border-2 border-grey rounded" name="query" />
+            <button type="submit" className="p-2 bg-white/20">Search</button>
+        </form>
+    );
+}
---
+++ src/app/datail/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='className="flex min-h-screen flex-col items-center justify-between p-24"'>Detalle de la Accion</div>
+    )
+}
+
+export default page
