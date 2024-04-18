501e1dc3e536726a0e2b4fde6c4f1ae622fa7560
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-18 08:35:39 -0300

    Se aplica el debounce

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,26 +1,32 @@
 'use client'
 
 import { useRouter, useSearchParams, redirect } from "next/navigation";
+import { useState, useEffect } from 'react'
 
 export default function SearchBar() {
     const router = useRouter();
     const searchParams = useSearchParams();
+    const [searchQuery, setSearchQuery] = useState(searchParams.get('q') || '');
 
-    function handleSubmit(event: any) {
-        const query = event.target.value;
-        router.push(`/?q=${query}`);
+    useEffect(() => {
+        const debounceTimeout = setTimeout(() => {
+            router.push(`/?q=${searchQuery}`);
+        }, 300);
+
+        return () => clearTimeout(debounceTimeout);
+    }, [searchQuery, router]);
+
+    const handleInputChange = (event: any) => {
+        setSearchQuery(event.target.value)
+
     }
 
     return (
         <>
             <form className="inline-flex gap-2 mb-4">
-                <input className="px-2 border-solid border-2 border-grey rounded" onChange={(e: any) => handleSubmit(e)} type="text" value={searchParams.get('q') || ''} />
+                <input className="px-2 border-solid border-2 border-grey rounded" onChange={handleInputChange} type="text" value={searchQuery} />
                 <label>Search</label>
             </form>
         </>
-        // <form onSubmit={handleSubmit} className="inline-flex gap-2 mb-4">
-        //     <input defaultValue={searchParams.get('q') || ''} className="px-2 border-solid border-2 border-grey rounded" name="query" />
-        //     <button type="submit" className="p-2 bg-white/20">Search</button>
-        // </form>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ import SearchBar from './components/SearchBar';
 import { ActionsTable } from './components/Table';
 import { Suspense } from 'react';
 import { redirect } from 'next/navigation'
+import { useDebouncedCallback } from "use-debounce";
 
 export default async function Home({ searchParams }: { searchParams: { q: string } }) {
+
+
   const data = await api.search(searchParams.q)
 
   const currentPageData = data.slice(0, 50);
@@ -15,8 +18,6 @@ export default async function Home({ searchParams }: { searchParams: { q: string
   if (data.length === 0) {
     redirect('/?q=')
   }
-
-
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
