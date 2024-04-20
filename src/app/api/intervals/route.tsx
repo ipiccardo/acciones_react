bbcbe34c1bdee5d4fdf92e1a3a609ccd6f969636
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-20 11:26:23 -0300

    Arreglos finales

--- src/app/api/intervals/route.tsx
+++ src/app/api/intervals/route.tsx
@@ -14,7 +14,7 @@ export async function GET(request: NextRequest) {
 
     // const res = await fetch(`https://api.twelvedata.com/time_series?symbol=${symbol}&interval=${interval}&start_date=2021-04-16%2009:48:00'&end_date=2021-04-16%2019:48:00&apikey=${apiKey}`,
 
-    const res = await fetch(`https://api.twelvedata.com/time_series?symbol=${symbol}&interval=${interval}&start_date=${date}&end_date=${endDate}&apikey=${apiKey}`,
+    const res = await fetch(`https://api.twelvedata.com/time_series?symbol=${symbol}&interval=${interval}&start_date=${date}&${endDate && `end_date=${endDate}`}&apikey=${apiKey}`,
 
     )
     const data = await res.json()
